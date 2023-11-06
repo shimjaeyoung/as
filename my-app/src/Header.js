a4722c2b48886a7396cf5760071f5ecba12eca53
shim jae young <tian9561@naver.com>
2023-11-06 20:31:32 +0900

    살려줘

---
+++ my-app/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header ClassName="header">
+        <span>My Todo List</span>
+        <span>React</span>
+    </header>
+  );
+}
+
+export default Header;
